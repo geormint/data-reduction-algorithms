c66a57938e5ebe413903b698e95a057e64f37171
GM173 <59773696+GM173@users.noreply.github.com>
2023-11-30 22:30:11 +0200

    Update Condensed Nearest Neighbor.py

--- Condensed Nearest Neighbor.py
+++ Condensed Nearest Neighbor.py
@@ -19,18 +19,19 @@ def dists(testRow, trainRow):
         dist = distance_functions[calculation](f_row, s_row)
     else:
         print("No valid option")
+        
     return dist
 
-def getNeighbors(trainSet, value):
-    testRow = train[loops]
-    distances = [(trainRow, dists(testRow, trainRow)) for trainRow in train if trainRow != testRow]
-    distances.sort(key = lambda tup: tup[1]
-    if distances[0][0][-1] != testRow[-1]:
-	condensedSet.append(testRow)
-    return distances, condensedSet
+def get_neighbors(testRow):
+    distances = [(trainRow, dists(testRow, trainRow)) for trainRow in condensed_set]
+    distances.sort(key=lambda tup: tup[1])
+    
+    if distances and distances[0][0][-1] != testRow[-1]:
+        condensed_set.append(testRow)
+    return distances, condensed_set
 
 paths = []
-condensedSet = []
+condensed_set = []
 
 calculations = {
     1: "Euclidean",
@@ -44,91 +45,55 @@ calculation = int(input(f"Which distance you want to calculate - {', '.join([f'{
 parameter = None
 if calculation == 3:
     parameter = int(input("Choose parameter for Minkowski - 3 or 4: "))
-else:
-    exit()
 
 paths = [dirs for dirs in os.listdir() if dirs != os.path.basename(__file__)]
 
 for folder in paths:
-    stop = True
     reductionRate = 0
     base_dir = folder
     directory = os.listdir(base_dir)
-    results_file = open(f"{folder}-resultsENN.txt", "w")
     
     for file_count, files in enumerate(directory, start=1):
         path = os.path.join(base_dir, files)
         dataset = pd.read_csv(path, sep='\t', header=None, dtype=str)
+        train_set = dataset.values.tolist()
+        condensed_set.clear()
 
-        trainSet = dataset.values.tolist()	
-		condensedSet.clear() 
+        randChoice = random.choice(train_set)
+        condensed_set.append(randChoice)
+        train_set.remove(randChoice)
 
-		fileCount += 1
-		condensedFile = open(f"{folder} + '-tr' + {fileCount}", 'w')
-		
-		trainCount = 0
-		
-		for count in trainSet:
-			trainCount += 1
+        while True:
+            previous_data = len(train_set)
 
-		randChoice = random.choice(trainSet)
-		condensedSet.append(randChoice)
-		trainSet.remove(randChoice)
+            tic = time.perf_counter()
+            for value in train_set:
+                distances, condensed_set = get_neighbors(value)
+            toc = time.perf_counter()
+            
+            train_set = [value for value in train_set if value not in condensed_set]
 
-		previousData = 0
-		condensedCount = 0
+            current_data = len(train_set)
+        
+            if previous_data == current_data:
+                break
 
-		while stop:
-			dataCount = 0
-			currentData = 0
+        with open(f"{folder}-tr{file_count}.txt", "w") as condensed_file:
+            for element in condensed_set:
+                condensed_file.write('\t'.join(map(str, element)) + '\n')
+        
+        rate = (1 - (len(condensed_set) / len(train_set))) * 100.0
 
-			for _ in trainSet:
-				dataCount += 1
-
-			tic = time.perf_counter()
-			for value in range(dataCount):
-				print(value)
-				distances, condensedSet = getNeighbors(trainSet, value)
-			toc = time.perf_counter()
-
-			for element in condensedSet:
-				for value in trainSet:
-					if element == value:
-						trainSet.remove(element)
-
-			for _ in trainSet:
-				currentData += 1
-
-			if previousData == currentData:
-				stop = False
-
-			previousData = currentData
-		
-		length = len(condensedSet[0])
-
-		for element in condensedSet:
-			for index in range(length):
-				condensedFile.write(str(element[index]))
-				if index != length - 1:
-					condensedFile.write('\t')
-				else:
-					continue
-			condensedCount += 1 
-			condensedFile.write("\n")
-
-
-		condensedFile.close()
-
-		rate = (1 - (condensedCount / trainCount)) * 100.0
-	results_file.write(
-            f"\n\n\n\nResults of file {file_count}"
-            f"\nTime seconds: {toc - tic}"
-            f"\nTraining set: {trainCount}\tEdited set: {editedCount}"
-            f"\nPercentage difference: {rate}"
-        )
+        with open(f"{folder}-resultsCNN.txt", "a") as results_file:
+            results_file.write(
+                f"\n\n\n\nResults of file {file_count}"
+                f"\nTime seconds: {toc - tic}"
+                f"\nTraining set: {len(train_set)}\tCondensed set: {len(condensed_set)}"
+                f"\nPercentage difference: {rate}"
+            )
 
         reductionRate += rate
 
-r_rate = reductionRate / 5
-results_file.write(f"\n\n\nReduction rate: {r_rate}")
-results_file.close()
+    r_rate = reductionRate / 5
+    with open(f"{folder}-resultsCNN.txt", "a") as results_file:
+        results_file.write(f"\n\n\nReduction rate: {r_rate}")
